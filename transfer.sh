b23d2a902a1330fe108c776ff1af358f0f950423
Patrick Ryan <12dcode@gmail.com>
2017-06-14 00:27:27 -0700

    Fixing transfer

--- transfer.sh
+++ transfer.sh
@@ -1,3 +1,5 @@
-rsync -avPz . cultstate.com:~/tech-career
+rsync -avPz --exclude={data/player/*.json,data/account/*.json} . cultstate.com:~/tech-career
 ssh cultstate.com 'killall node'
 ssh cultstate.com 'export NVM_DIR=~/.nvm && source ~/.nvm/nvm.sh && node /home/cultstate/tech-career/ranvier </dev/null >/home/cultstate/tech-career.log 2>&1 &'
+echo "Server rebooted, Ctrl + C to quit!
+
