567d78707611a2277163dbc263d6ff2a66f1e174
pewpewitsatiger <jaydon.montalvo@embelm21.com>
2017-07-14 16:25:15 -0400

    Linked the damage spell to the programmer class

--- bundles/ranvier-classes/classes/programmer.js
+++ bundles/ranvier-classes/classes/programmer.js
@@ -4,8 +4,9 @@ module.exports = () => {
     name: "Programmer",
     description: "Programmer spend years learning to harness digital forces to impose their will on the world around them. As techies, they tend to be less influential than Managers and less stout than Support. Their powerful scripts keep them alive and allow them to wreak havoc... as long as they have the focus to write them.",
     abilityTable: {
-      1: { spells: ["doxx"]},
-      2: { spells: ["cripple"]},
+      1: { spells: ["damage"]},
+      2: { spells: ["doxx"]},
+      3: { spells: ["cripple"]},
       5: { spells: ["hack"] }
     },
 
